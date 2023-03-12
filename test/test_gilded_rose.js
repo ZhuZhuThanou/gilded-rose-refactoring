70fb1435decb5649f7bffca4bc8ca423d118224b
ZhuZhuThanou <blogicalbmath@gmail.com>
2023-03-12 19:08:44 -0400

    commit initial code

---
+++ test/test_gilded_rose.js
@@ -0,0 +1,9 @@
+var { assert, expect } = require("chai");
+
+var { Shop, Item } = require("../gilded_rose.js");
+
+describe("Gilded Rose", function () {
+  it("should test", function () {
+    assert.isOk('everyting is okay');
+  });
+});
